6506154751dc7d06243b10773120a121ca16d0b5
Derek Lim <dereklim3006@gmail.com>
2026-02-01 16:06:55 -0500

    perf(ex2): serve responsive AVIF images

--- src/components/PhotoGridItem/PhotoGridItem.jsx
+++ src/components/PhotoGridItem/PhotoGridItem.jsx
@@ -2,10 +2,25 @@ import React from 'react';
 import styled from 'styled-components';
 
 const PhotoGridItem = ({ id, src, alt, tags }) => {
+  const avifSet = `
+    ${src.replace('.jpg', '.avif')} 1x,
+    ${src.replace('.jpg', '@2x.avif')} 2x,
+    ${src.replace('.jpg', '@3x.avif')} 3x
+  `;
+  const jpgSet = `
+    ${src} 1x,
+    ${src.replace('.jpg', '@2x.jpg')} 2x,
+    ${src.replace('.jpg', '@3x.jpg')} 3x
+  `;
+
   return (
     <article>
       <Anchor href={`/photos/${id}`}>
-        <Image src={src} />
+        <picture>
+          <source srcSet={avifSet} />
+          <source srcSet={jpgSet} />
+          <Image src={src} />
+        </picture>
       </Anchor>
       <Tags>
         {tags.map((tag) => (
@@ -28,6 +43,7 @@ const Image = styled.img`
   height: 300px;
   border-radius: 2px;
   margin-bottom: 8px;
+  object-fit: cover;
 `;
 
 const Tags = styled.ul`
